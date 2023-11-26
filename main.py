6c5ca682a514368f9c5de6655448f55584393370
Luiz Santiago <123255082+luizsant@users.noreply.github.com>
2023-11-26 16:48:02 -0300

    subindo com classes abstratas e property

--- main.py
+++ main.py
@@ -1,10 +1,89 @@
-print('''
-### BEM VINDO AO BANCO CHOMPIRAS S/A ###
-''')
+import re
 
-value = 10.50
-saqueNumber = 0
-operacoes = []
+class Cliente:
+    def __init__(self, nome, cpf):
+        self.nome = nome
+        self.cpf = cpf
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, valor):
+        if not all(x.isalpha() or x.isspace() for x in valor):
+            raise ValueError("Nome inválido.")
+        self._nome = valor
+
+    @property
+    def cpf(self):
+        return self._cpf
+
+    @cpf.setter
+    def cpf(self, valor):
+        if not self.validar_cpf(valor):
+            raise ValueError("CPF inválido.")
+        self._cpf = self.formatar_cpf(valor)
+
+    @staticmethod
+    def validar_cpf(cpf):
+        # Simplificado: Adicione aqui sua lógica de validação de CPF
+        return bool(re.match(r'^\d{11}$', cpf))
+
+    @staticmethod
+    def formatar_cpf(cpf):
+        return f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+
+class Banco:
+    def __init__(self):
+        self.clientes = []
+        self.operacoes = []
+        self.saldo = 0.0
+        self.saqueNumber = 0
+
+    def cadastrar_cliente(self, nome, cpf):
+        try:
+            cliente = Cliente(nome, cpf)
+            self.clientes.append(cliente)
+            print("Cliente cadastrado com sucesso.")
+        except ValueError as e:
+            print(e)
+
+    def excluir_cliente(self, cpf):
+        cpf_formatado = Cliente.formatar_cpf(cpf)
+        self.clientes = [cliente for cliente in self.clientes if cliente.cpf != cpf_formatado]
+        print("Cliente excluído com sucesso.")
+
+    def deposito(self, valor):
+        if valor > 0:
+            self.saldo += valor
+            self.operacoes.append(f'Depósito: R${valor:.2f}')
+            print(f'Seu novo saldo é R${self.saldo:.2f}')
+        else:
+            print('Valor inválido para depósito.')
+
+    def saque(self, valor):
+        if self.saqueNumber < 3 and 0 < valor <= self.saldo and valor <= 500:
+            self.saldo -= valor
+            self.saqueNumber += 1
+            self.operacoes.append(f'Saque: R${valor:.2f}')
+            print(f'Seu novo saldo é R${self.saldo:.2f}')
+            print(f'Saques restantes para hoje: {3 - self.saqueNumber}')
+        else:
+            print('Valor inválido ou limite de saques diários excedido.')
+
+    def extrato(self):
+        if not self.operacoes:
+            print("Nenhuma operação foi realizada.")
+        else:
+            print("Histórico de operações:")
+            for operacao in self.operacoes:
+                print(operacao)
+
+    def saldo_atual(self):
+        print(f'Seu saldo é de R${self.saldo:.2f}')
+
+banco = Banco()
 
 while True:
     option = int(input('''
@@ -13,42 +92,31 @@ Digite a opção desejada:
 2. Depósito
 3. Saque
 4. Saldo
-5. Sair
+5. Cadastrar Cliente
+6. Excluir Cliente
+7. Sair
 
 -> Opção: 
 '''))
 
     if option == 1:
-        if not operacoes:
-            print("Nenhuma operação foi realizada.")
-        else:
-            print("Histórico de operações:")
-            for operacao in operacoes:
-                print(operacao)
+        banco.extrato()
     elif option == 2:
-        cash = float(input('Digite o valor que você quer depositar: '))
-        if cash > 0:
-            value += cash
-            operacoes.append(f'Depósito: R${cash:.2f}')
-            print(f'Seu novo saldo é R${value:.2f}')
-        else:
-            print('Operação encerrada.')
+        valor = float(input('Digite o valor que você quer depositar: '))
+        banco.deposito(valor)
     elif option == 3:
-        if saqueNumber < 3:
-            cash = float(input('Digite o valor que você quer sacar: '))
-            if 0 < cash <= value and cash <= 500:
-                value -= cash
-                saqueNumber += 1
-                operacoes.append(f'Saque: R${cash:.2f}')
-                print(f'Seu novo saldo é R${value:.2f}')
-                print(f'Saques restantes para hoje: {3 - saqueNumber}')
-            else:
-                print('Valor acima do limite de R$500,00 ou saldo insuficiente.')
-        else:
-            print('Você excedeu seu limite diário de saques')
+        valor = float(input('Digite o valor que você quer sacar: '))
+        banco.saque(valor)
     elif option == 4:
-        print(f'Seu saldo é de R${value:.2f}')
+        banco.saldo_atual()
     elif option == 5:
+        nome = input("Digite o nome do cliente: ")
+        cpf = input("Digite o CPF do cliente (apenas números): ")
+        banco.cadastrar_cliente(nome, cpf)
+    elif option == 6:
+        cpf = input("Digite o CPF do cliente a ser excluído (apenas números): ")
+        banco.excluir_cliente(cpf)
+    elif option == 7:
         print('Obrigado por usar o Banco Chompiras. Até logo!')
         break
     else:
